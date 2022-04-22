6384c436b9c29e43211b6a807ba117cda8fb4330
Visnu <vgcaisse@gmail.com>
2022-04-22 18:55:17 -0400

    init commit, added depps and scripts

--- api/server.js
+++ api/server.js
@@ -1,1 +1,9 @@
 // build your server here and require it from index.js
+const server = require('./api/server.js');
+
+const PORT = process.env.PORT || 9000;
+
+server.listen(PORT, () => {
+    console.log(`Revved up on port: ${PORT}`);
+});
+
